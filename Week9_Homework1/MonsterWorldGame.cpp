e38c391f1fb505a641c2f5217d4213f8a86372cd
miinnnhho <102772287+miinnnhho@users.noreply.github.com>
2022-05-20 09:09:59 +0900

    Add files via upload

---
+++ Week9_Homework1/MonsterWorldGame.cpp
@@ -0,0 +1,18 @@
+#include "MonsterWorld.h"
+#include "VariousMonsters.h"
+#include <time.h>
+
+void main()
+{
+	srand((unsigned int)time(NULL));
+	int w = 16, h = 8;
+	MonsterWorld game(w, h);
+
+	game.add(new Zombie("Á¶¹ÎÈ£", "¡İ", rand() % w, rand() % h));
+	game.add(new Vampire("ÀÌ¸íÁØ", "¡Ú", rand() % w, rand() % h));
+	game.add(new KGhost("ÀÌÈñ¿õ", "¢¾", rand() % w, rand() % h));
+	game.add(new Jiangshi("¹Ú¹ÎÀç", "¡ê", rand() % w, rand() % h, true));
+	game.add(new Jiangshi("ÀÌÈñ¼®", "¢Õ", rand() % w, rand() % h, false));
+	game.play(500, 10);
+	printf("------°ÔÀÓ Á¾·á-------------------\n");
+}
